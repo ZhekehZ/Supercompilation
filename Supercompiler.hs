4ef102af5c32558532b2ee11560d10d9a010fd15
Eugene Kravchenko <zzzheka97@gmail.com>
2020-11-28 23:24:01 +0300

    upd

--- Supercompiler.hs
+++ Supercompiler.hs
@@ -1,13 +1,15 @@
 module Supercompiler where
 
+import Control.Applicative
+
 type Name = String
                                                                    -- Term example
-data Expr val = Val val                                            -- 1
-              | ValF { arity :: Int                                -- +
-                     , builtinFunction :: [val] -> val
+data Expr val = Val  { getValue :: val }                           -- 1
+              | ValF { builtinFunction :: [val] -> val             -- +
+                     , arguments :: [Expr val]
                      }
-              | ValP { arity :: Int                                -- ==
-                     , builtinPredicate :: [val] -> Bool
+              | ValP { builtinPredicate :: [val] -> Bool           -- ==
+                     , arguments :: [Expr val]
                      }
               | Var { varName :: Name }                            -- x
               | Con { constructor :: Name                          -- C (x1, ..., xn)
@@ -26,17 +28,33 @@ data Expr val = Val val                                            -- 1
 infix 5 :=>
 infixr 4 :->
 
+instance Show val => Show (Expr val) where
+  show (Val v) = "Val " ++ show v
+  show (ValF v args) = "ValF ?? " ++ show args
+  show (ValP v args) = "ValP ?? " ++ show args
+  show (Var v) = "Var " ++ v
+  show (Con c args) = "Con " ++ c ++ " " ++ show args
+  show (Fun f) = "Fun " ++ f
+  show (a:->b) = "\\" ++ a ++ " -> " ++ show b
+  show (a:@b) = "(" ++ show a ++ ")(" ++ show b ++ ")"
+  show (Case e xs) = "Case " ++ show e ++ show xs
+  show (Let x e1 e2) = "Let " ++ x ++ " " ++ show e1 ++ " in " ++ show e2
+
 data PMCase val = Pattern :=> Expr val
+  deriving Show
 
 data Pattern = Pat { pConstructor :: Name
                    , pConstructorVariables :: [Name]
                    }
+  deriving Show
 
 data Def val = Def Name (Expr val)
+  deriving Show
 
 data Program val = Program {  programDefinitions :: [Def val]
                            ,  entryPoint         :: Name
                            }
+  deriving Show
 
 
 testProgram :: Program Int
@@ -44,19 +62,19 @@ testProgram = Program { programDefinitions =
                           [ Def "sum" $ "xs" :-> "a" :->
                                 Case (Var "xs")
                                 [ Pat "Nil"  []          :=> Var "a"
-                                , Pat "Cons" ["x", "xs"] :=> Fun "sum" :@ Var "xs" :@ (plus :@ Var "x" :@ Val 0)
+                                , Pat "Cons" ["x", "xs"] :=> Fun "sum" :@ Var "xs" :@ (plus [Var "x", Val 0])
                                 ]
 
                           , Def "squares" $ "xs" :->
                                 Case (Var "xs")
                                 [ Pat "Nil"  []          :=> Con "Nil" []
-                                , Pat "Cons" ["x", "xs"] :=> Con "Cons" [mul :@ Var "x" :@ Var "x", Fun "squares" :@ Var "xs"]
+                                , Pat "Cons" ["x", "xs"] :=> Con "Cons" [mul [Var "x", Var "x"], Fun "squares" :@ Var "xs"]
                                 ]
 
                           , Def "upto" $ "m" :-> "n" :->
-                                Case (gt :@ Var "m" :@ Var "n")
+                                Case (gt [Var "m", Var "n"])
                                 [ Pat "True"  [] :=> Con "Nil" []
-                                , Pat "True"  [] :=> Con "Cons" [Var "m", Fun "upto" :@ (plus :@ Var "m" :@ Val 1) :@ Var "n"]
+                                , Pat "True"  [] :=> Con "Cons" [Var "m", Fun "upto" :@ (plus [Var "m", Val 1]) :@ Var "n"]
                                 ]
 
                           , Def "main" $ Fun "sum" :@ (Fun "squares" :@ (Fun "upto" :@ Val 1 :@ Var "arg0")) :@ Val 1
@@ -64,13 +82,12 @@ testProgram = Program { programDefinitions =
                       , entryPoint = "main"
                       }
       where
-          [plus, mul] = map (\f -> ValF 2 $ \[x, y] -> f y x) [(+), (*)]
-          [gt]        = map (\f -> ValP 2 $ \[x, y] -> f y x) [(>)]
-
+          [plus, mul] = map (\f -> ValF $ \[x, y] -> f x y) [(+), (*)]
+          [gt]        = map (\f -> ValP $ \[x, y] -> f x y) [(>)]
 
 type Context val = [(Name, Expr val)]
 
-evalProgram :: Program val -> Context val -> Expr val
+evalProgram :: Program val -> Context val -> Maybe (Expr val)
 evalProgram Program {programDefinitions=def, entryPoint=entry} args = evalExpr args def (lookupFun def entry)
 
 lookupFun :: [Def val] -> Name -> Expr val
@@ -78,19 +95,38 @@ lookupFun (Def name' expr : defs) name | name == name' = expr
                                        | otherwise     = lookupFun defs name
 lookupFun [] name = error $ "Invalid function name: " ++ name
 
-evalExpr :: Context val -> [Def val] -> Expr val -> Expr val
-evalExpr c d (Val v) = Val v
-evalExpr c d (ValF a builtinF) = Val $ builtinF (map (\x -> case x of (_, Val v) -> v) $ take a c)
-evalExpr c d (ValP a builtinP) = Con (if builtinP (map (\x -> case x of (_, Val v) -> v) $ take a c) then "True" else "False") []
-evalExpr c d (Var name) = case lookup name c of
-                            Just e -> e
-                            Nothing -> error $ "Invalid variable name (" ++ name ++ ")"
-evalExpr c d (Con cname args) = undefined
-evalExpr c d (Fun f) = undefined
-evalExpr c d (n :-> e)  = undefined
-evalExpr c d (e1 :@ e2) = undefined
-evalExpr c d (Case e ps) = undefined
-evalExpr c d (Let x v e) = undefined
+evalExpr' c d e = maybe e (evalExpr' c d) $ evalExpr c d e
+
+subst :: Expr val -> Name -> Expr val -> Expr val
+subst e n u = case u of
+    Val v       -> Val v
+    ValF f args -> ValF f (subst' <$> args)
+    ValP p args -> ValP p (subst' <$> args)
+    Var x       -> if x == n then e else Var x
+    Con c args  -> Con c (subst' <$> args)
+    Fun f       -> Fun f
+    x :-> t     -> undefined
+    e1 :@ e2    -> subst' e1 :@ subst' e2
+    Case e pmc  -> undefined
+    Let x e t   -> undefined
+    where
+        subst' = subst e n
+
+evalExpr :: Context val -> [Def val] -> Expr val -> Maybe (Expr val)
+evalExpr context defines expression = case expression of
+  ValF f args -> (ValF f <$> evalArguments args) <|> (Just $ Val $ f (getValue <$> args))
+  ValP p args -> (ValP p <$> evalArguments args) <|> (Just $ Con (show $ p (getValue <$> args)) [])
+  Var v       -> lookup v context
+  Fun f       -> Just $ lookupFun defines f
+  ((x:->e):@t)-> Just $ subst t x e
+  (Let x e t) -> (\e -> subst e x t) <$> eval e
+  (e1:@e2)    -> (:@e2) <$> eval e1
+  _ -> Nothing
+
+  where
+      eval = evalExpr context defines
+      evalArguments (x:xs) = ((:xs) <$> eval x) <|>  ((x:) <$> evalArguments xs)
+      evalArguments []     = Nothing
 
 --main :: IO ()
 --main = putStrLn (show (programDefinitions testProgram))
